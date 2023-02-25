be4451835f471dbe4f9de7927ff06df5353783f9
TheRook <firealwaysworks@gmail.com>
2023-02-25 12:54:41 -0800

    added GTK4 and NCBI fetch
    You can now fetch a gene by its name and it will save a correctly annotated genbank file from NCBI which normally gives you FASTA. 
    
    We are now using GTK4 to discover new mutations.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
-from Bio import SeqIO, SeqFeature
-from Bio.SeqFeature import SeqFeature, FeatureLocation
 import typer
-from Bio.Seq import Seq
-from Bio.Restriction import SmaI
+import subprocess
+import requests
+from Bio import SeqIO, Entrez
+from Bio.Restriction import SmaI, RestrictionBatch
 from Bio.SeqFeature import FeatureLocation, CompoundLocation
-from Bio.Restriction import RestrictionBatch
-
 
 app = typer.Typer()
 
@@ -53,19 +51,120 @@ def generate_sgRNA_targets(genome_file: str, target_dir: str, output_file: str):
         for target in potential_targets:
             f.write(f"{genome_record.id},{target}\n")
 
-@app.command()
-def generate_sgRNA(
-    genome_file: str = typer.Argument(..., help="Path to genome file in FASTA format"),
-    target_dir: str = typer.Argument(..., help="Path to directory containing genbank files specifying target regions"),
-    output_file: str = typer.Argument(..., help="Path to output file"),
-):
+def get_mutation_data(gene_name):
     """
-    Generate sgRNA targets based off of a full genome input and a list of genbank target files
+    Retrieves mutation data for a given gene from an external data source.
+
+    Args:
+        gene_name (str): Name of the gene to retrieve mutation data for.
+
+    Returns:
+        List of dictionaries, where each dictionary contains information about a single mutation.
     """
-    generate_sgRNA_targets(genome_file, target_dir, output_file)
+    url = f"https://magi.brown.edu/api/gene?hugo_symbol={gene_name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        mutations = []
+
+        for mutation in data["mutations"]:
+            mutation_data = {
+                "amino_acid_change": mutation["amino_acid_change"],
+                "chromosome": mutation["chromosome"],
+                "start": mutation["start"],
+                "stop": mutation["stop"],
+                "reference": mutation["reference"],
+                "variant": mutation["variant"],
+                "classification": mutation["classification"],
+            }
+            mutations.append(mutation_data)
+
+        return mutations
+    else:
+        return None
 
 @app.command()
-def compile_plasmid(
+def download_gene_data(gene_name: str, email: str = typer.Option(None, help="Email address for NCBI queries")):
+    """
+    Downloads gene data for a given gene name from NCBI and saves all fields as annotations in a genbank file.
+    """
+    # If email is not provided, use a hard-coded email address
+    if email is None:
+        email = "your.email@example.com"
+    
+    # Set the email address for Entrez queries
+    Entrez.email = email
+    
+    # Search for the gene name in NCBI's nucleotide database
+    handle = Entrez.esearch(db="nucleotide", term=gene_name)
+    record = Entrez.read(handle)
+    handle.close()
+
+    # If at least one record is found, download the first one and save all fields as annotations
+    if record["IdList"]:
+        handle = Entrez.efetch(db="nucleotide", id=record["IdList"][0], rettype="gb", retmode="text")
+        gene_record = SeqIO.read(handle, "gb")
+        handle.close()
+
+        # Retrieve mutation data from MAGI - todo MAGI down.
+        mutation_data = None #get_mutation_data(gene_name)
+
+        # If there is mutation data, add it as an annotation to the gene record
+        if mutation_data:
+            for feature in gene_record.features:
+                if feature.type == "gene":
+                    feature.qualifiers["mutation_data"] = str(mutation_data)
+                    break
+
+        # Add annotations for gene name, accession, and taxonomy
+        gene_record.annotations["gene_name"] = gene_name
+        gene_record.annotations["source"] = "NCBI"
+        check_accession = gene_record.id.split(":")
+        if len(check_accession)>=2:
+            gene_record.annotations["accession"] = check_accession[1]
+        
+        # Save the gene record as a genbank file with the gene name as the filename
+        SeqIO.write(gene_record, f"{gene_name}.gb", "gb")
+        typer.echo(f"Gene data for {gene_name} downloaded and saved to {gene_name}.gb")
+    else:
+        typer.echo(f"No records found for {gene_name} in NCBI's nucleotide database")
+
+@app.command()
+def find_mutations(reference_file: str = typer.Argument(..., help="Path to the reference genome FASTA file."),
+                   gene_name: str = typer.Argument(..., help="Name of the gene of interest."),
+                   output_file: str = typer.Argument(..., help="Path to the output VCF file.")):
+    """
+    Use GATK4 to find all mutations of a given gene within a genome.
+    """
+    # Use Conda to activate the GATK4 environment
+    cmd = 'conda activate gatk4'
+    subprocess.run(cmd, shell=True)
+
+    # Use Biopython to get the gene coordinates
+    reference_seq = SeqIO.read(reference_file, 'fasta')
+    gene_seq = reference_seq.seq
+    for feature in reference_seq.features:
+        if feature.type == 'gene' and feature.qualifiers['gene'][0] == gene_name:
+            start = feature.location.start.position
+            end = feature.location.end.position
+            gene_seq = feature.extract(reference_seq.seq)
+
+    # Call variants using GATK4 HaplotypeCaller
+    tmp_vcf = 'tmp.vcf'
+    cmd = f'gatk HaplotypeCaller -R {reference_file} -I {reference_file} -L {gene_name} -O {tmp_vcf}'
+    subprocess.run(cmd, shell=True)
+
+    # Filter variants to retain only those overlapping with the specified gene
+    cmd = f'bcftools view {tmp_vcf} -T {start}:{end} -O v -o {output_file}'
+    subprocess.run(cmd, shell=True)
+
+    # Clean up temporary files
+    os.remove(tmp_vcf)
+
+
+@app.command()
+def modify_plasmid(
     backbone_file: str = typer.Option(..., "--backbone-file", "-b", help="The path to the GenBank file of the plasmid backbone to build from."),
     target_dir: str = typer.Option(..., "--target-dir", "-t", help="The path to the directory containing the list of target genes to replace."),
     replacement_dir: str = typer.Option(..., "--replacement-dir", "-r", help="The path to the directory containing the list of replacement genes to use."),
